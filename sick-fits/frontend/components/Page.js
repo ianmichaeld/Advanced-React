6c6443830119ee38ec9016e318a8c1c9d8e1c9c8
Ian Dewsbury <ianmichaeld@gmail.com>
2018-11-09 15:46:31 -0500

    Add NProgress bar to Header component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -4,7 +4,7 @@ import Meta from './Meta'
 import styled, { ThemeProvider, injectGlobal } from 'styled-components'
 
 const theme = {
-  red: '#FF0000',
+  red: '#77aad5',
   black: '#393939',
   grey: '#3A3A3A',
   lightGrey: '#E1E1E1',
