b291582a0cfdc3ee36184263a4ae2c687ec10e7e
Ian Dewsbury <ianmichaeld@gmail.com>
2018-12-04 14:48:12 -0500

    Add change and submit handlers for CreateItem component

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -4,6 +4,26 @@ import gql from 'graphql-tag'
 import Form from './styles/Form'
 import formatMoney from '../lib/formatMoney'
 
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $image: String
+    $largeImage: String
+    $price: Int!
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      image: $image
+      largeImage: $largeImage
+      price: $price
+    ) {
+      id
+    }
+  }
+`
+
 export default class CreateItem extends Component {
   state = {
     title: '',
@@ -15,26 +35,60 @@ export default class CreateItem extends Component {
   handleChange = e => {
     const { name, type, value } = e.target
     const val = type === 'number' ? parseFloat(value) : value
-    console.log({ name, type, value, val })
     this.setState({ [name]: val })
   }
   render() {
     return (
-      <Form>
-        <fieldset>
-          <label htmlFor="Title">
-            <input
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              required
-              value={this.state.title}
-              onChange={this.handleChange}
-            />
-          </label>
-        </fieldset>
-      </Form>
+      <Mutation query={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error }) => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault()
+            }}
+          >
+            <fieldset>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter your description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
     )
   }
 }
+
+export { CREATE_ITEM_MUTATION }
