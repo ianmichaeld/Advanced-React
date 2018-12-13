21d9d30c41d82105743a865bc9597973dfcaa804
Ian Dewsbury <ianmichaeld@gmail.com>
2018-12-13 14:25:10 -0500

    Fix bug in updateItem mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -4,15 +4,17 @@ const Mutations = {
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info)
     return item
   },
-  async updateItem(parent, args, ctx, info) {
+  updateItem(parent, args, ctx, info) {
     const updates = { ...args }
     delete updates.id
-    const item = await ctx.db.mutation.updateItem(
-      { data: updates },
-      { where: { id: args.id } },
+    console.log(updates)
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: { id: args.id }
+      },
       info
     )
-    return item
   }
 }
 
